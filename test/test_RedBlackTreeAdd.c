3ea6689719d748cbc212e011c06fbd96190b7a45
mygamelife <mygamelife1@gmail.com>
2014-10-23 10:26:19 +0800

    removed all the spaces in command line

--- test/test_RedBlackTreeAdd.c
+++ test/test_RedBlackTreeAdd.c
@@ -24,24 +24,24 @@ void setUp(void)	{
 void tearDown(void) {}
 
 /*
- * Root->  NULL                10  <-Root                                         
+ * Root->  NULL                10  <-Root
  *                add 10
  *								---->
  */
 void test_addRedBlackTree_add_10_to_empty_tree(void)	{
 	setNode(&node10, NULL, NULL, 'r');
 	Node *root = NULL;
-	
+
 	printf("Start test_addRedBlacTree_add_10_to_empty_tree\n");
 	addRedBlackTree(&root, &node10);
 	printf("-----------------------------");
-  
+
 	TEST_ASSERT_EQUAL_PTR(root, &node10);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'b', &node10);
 }
 
 /*
- * Root->  	10(b)              		10(b)  <-Root                                         
+ * Root->  	10(b)              		10(b)  <-Root
  *          	      add 5     	/
  *									---->			5(r)
  */
@@ -49,19 +49,19 @@ void test_addRedBlackTree_add_5_to_tree_with_root_10(void)	{
 	setNode(&node10, NULL, NULL, 'b');
 	setNode(&node5, NULL, NULL, 'r');
 	Node *root = &node10;
-	
+
 	printf("Start test_addRedBlacTree_add_5_to_tree_with_root_10\n");
 	addRedBlackTree(&root, &node5);
 	printf("-----------------------------");
-  
+
 	TEST_ASSERT_EQUAL_PTR(root, &node10);
-	
+
 	TEST_ASSERT_EQUAL_NODE(&node5, NULL, 'b', &node10);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node5);
 }
 
 /*
- * Root->  	10(b)              		10(b)  <-Root                                         
+ * Root->  	10(b)              		10(b)  <-Root
  *          	      add 13     	    \
  *									---->	           13(r)
  */
@@ -69,13 +69,13 @@ void test_addRedBlackTree_add_13_to_tree_with_root_10(void)	{
 	setNode(&node10, NULL, NULL, 'b');
 	setNode(&node13, NULL, NULL, 'r');
 	Node *root = &node10;
-	
+
 	printf("Start test_addRedBlacTree_add_13_to_tree_with_root_10\n");
 	addRedBlackTree(&root, &node13);
 	printf("-----------------------------");
-  
+
 	TEST_ASSERT_EQUAL_PTR(root, &node10);
-	
+
 	TEST_ASSERT_EQUAL_NODE(NULL, &node13, 'b', &node10);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node13);
 }
@@ -92,13 +92,13 @@ void test_addRedBlackTree_add_13_to_tree_with_root_10_and_right_child_15(void)	{
 	setNode(&node15, NULL, NULL, 'r');
 	setNode(&node10, NULL, &node15, 'b');
 	Node *root = &node10;
-	
+
 	printf("Start test_addRedBlackTree_add_13_to_tree_with_root_10_and_right_child_15\n");
 	addRedBlackTree(&root, &node13);
 	printf("-----------------------------");
-  
+
 	TEST_ASSERT_EQUAL_PTR(root, &node13);
-	
+
 	TEST_ASSERT_EQUAL_NODE(&node10, &node15, 'b', &node13);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node10);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node15);
@@ -109,20 +109,20 @@ void test_addRedBlackTree_add_13_to_tree_with_root_10_and_right_child_15(void)	{
  *         /	         add 8     	   /							 rotate left		 /               rotate left        /   \
  *       5(r)      		 ---->	     5(r)        	       ---->       8(r)                ---->         5(r)    10(r)
  *                                  \    													/		               flip color
- *                                   8(r)												5(r)			
+ *                                   8(r)												5(r)
  */
 void test_addRedBlackTree_add_8_to_tree_with_root_10_and_left_child_5(void)	{
 	setNode(&node8, NULL, NULL, 'r');
 	setNode(&node5, NULL, NULL, 'r');
 	setNode(&node10, &node5, NULL, 'b');
 	Node *root = &node10;
-	
+
 	printf("Start test_addRedBlackTree_add_8_to_tree_with_root_10_and_left_child_5\n");
 	addRedBlackTree(&root, &node8);
 	printf("-----------------------------");
-  
+
 	TEST_ASSERT_EQUAL_PTR(root, &node8);
-	
+
 	TEST_ASSERT_EQUAL_NODE(&node5, &node10, 'b', &node8);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node5);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node10);
@@ -133,44 +133,44 @@ void test_addRedBlackTree_add_8_to_tree_with_root_10_and_left_child_5(void)	{
  *         	 \         add 20        \							 rotate left		     /   \
  *            10(r)		 ---->	        10(r)    	       ---->          8(r)    20(r)
  *                                  	\    					 flip color
- *                                     20(r)															
+ *                                     20(r)
  */
 void test_addRedBlackTree_add_20_to_tree_with_root_8_and_right_child_10(void)	{
 	setNode(&node20, NULL, NULL, 'r');
 	setNode(&node10, NULL, NULL, 'r');
 	setNode(&node8, NULL, &node10, 'b');
 	Node *root = &node8;
-	
+
 	printf("Start test_addRedBlackTree_add_20_to_tree_with_root_8_and_right_child_10\n");
 	addRedBlackTree(&root, &node20);
 	printf("-----------------------------");
-  
+
 	TEST_ASSERT_EQUAL_PTR(root, &node10);
-	
+
 	TEST_ASSERT_EQUAL_NODE(&node8, &node20, 'b', &node10);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node8);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node20);
 }
 
 /*
- * Root->  	10(b)              		  10(b)  <-Root                           
- *         	/         add 20       /	\					
+ * Root->  	10(b)              		  10(b)  <-Root
+ *         	/         add 20       /	\
  *       8(r)         ---->	     8(r)  20(r)
- *                   flip color               	  					 
- *                                   														
+ *                   flip color
+ *
  */
 void test_addRedBlackTree_add_20_to_tree_with_root_10_and_left_child_8(void)	{
 	setNode(&node8, NULL, NULL, 'r');
 	setNode(&node20, NULL, NULL, 'r');
 	setNode(&node10, &node8, NULL, 'b');
 	Node *root = &node10;
-	
+
 	printf("Start test_addRedBlackTree_add_20_to_tree_with_root_10_and_left_child_8\n");
 	addRedBlackTree(&root, &node20);
 	printf("-----------------------------");
-  
+
 	TEST_ASSERT_EQUAL_PTR(root, &node10);
-	
+
 	TEST_ASSERT_EQUAL_NODE(&node8, &node20, 'b', &node10);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node8);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node20);
@@ -181,43 +181,43 @@ void test_addRedBlackTree_add_20_to_tree_with_root_10_and_left_child_8(void)	{
  *         	/         add 5         /						      right rotate         /  \
  *       13(r)        ---->	      13(r)                  ---->           5(r)  15(r)
  *                               /   	  					    flip color
- *                              5(r)     														
+ *                              5(r)
  */
 void test_addRedBlackTree_add_5_to_tree_with_root_15_and_left_child_13(void)	{
 	setNode(&node13, NULL, NULL, 'r');
 	setNode(&node5, NULL, NULL, 'r');
 	setNode(&node15, &node13, NULL, 'b');
 	Node *root = &node15;
-	
+
 	printf("Start test_addRedBlackTree_add_5_to_tree_with_root_15_and_left_child_13\n");
 	addRedBlackTree(&root, &node5);
 	printf("-----------------------------");
-  
+
 	TEST_ASSERT_EQUAL_PTR(root, &node13);
-	
+
 	TEST_ASSERT_EQUAL_NODE(&node5, &node15, 'b', &node13);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node5);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node15);
 }
 
 /*
- * Root->  	13(b)              		   13(b)  <-Root  
- *         	  \        add 5        /	 \					      
+ * Root->  	13(b)              		   13(b)  <-Root
+ *         	  \        add 5        /	 \
  *             15(r)   ---->	    5(r)  15(r)
- *                                   														
+ *
  */
 void test_addRedBlackTree_add_5_to_tree_with_root_13_and_right_child_15(void)	{
 	setNode(&node5, NULL, NULL, 'r');
 	setNode(&node15, NULL, NULL, 'r');
 	setNode(&node13, NULL, &node15, 'b');
 	Node *root = &node13;
-	
+
 	printf("Start test_addRedBlackTree_add_5_to_tree_with_root_13_and_right_child_15\n");
 	addRedBlackTree(&root, &node5);
 	printf("-----------------------------");
-  
+
 	TEST_ASSERT_EQUAL_PTR(root, &node13);
-	
+
 	TEST_ASSERT_EQUAL_NODE(&node5, &node15, 'b', &node13);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node5);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node15);
@@ -228,7 +228,7 @@ void test_addRedBlackTree_add_5_to_tree_with_root_13_and_right_child_15(void)	{
  *         /	\        add 5        /	 \					     flip color          /  \
  *       8(r)  15(r)   ---->	    8(r)  15(r)            ---->          8(b)  15(b)
  *                               /                                     /
- *                             5(r)     													   5(r)   
+ *                             5(r)     													   5(r)
  */
 void test_addRedBlackTree_add_5_to_tree_with_root_10_and_left_child_8_right_child_15(void)	{
 	setNode(&node5, NULL, NULL, 'r');
@@ -236,13 +236,13 @@ void test_addRedBlackTree_add_5_to_tree_with_root_10_and_left_child_8_right_chil
 	setNode(&node15, NULL, NULL, 'r');
 	setNode(&node10, &node8, &node15, 'b');
 	Node *root = &node10;
-	
+
 	printf("Start test_addRedBlackTree_add_5_to_tree_with_root_10_and_left_child_8_right_child_15\n");
 	addRedBlackTree(&root, &node5);
 	printf("-----------------------------");
-  
+
 	TEST_ASSERT_EQUAL_PTR(root, &node10);
-	
+
 	TEST_ASSERT_EQUAL_NODE(&node8, &node15, 'r', &node10);
 	TEST_ASSERT_EQUAL_NODE(&node5, NULL, 'b', &node8);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node5);
@@ -253,8 +253,8 @@ void test_addRedBlackTree_add_5_to_tree_with_root_10_and_left_child_8_right_chil
  * Root->  	10(b)              		   10(b)  <-Root                          10(r)
  *         /	\        add 8        /	 \					     flip color          /  \
  *       5(r)  15(r)   ---->	    5(r)  15(r)            ---->          5(b)  15(b)
- *                                  \                                    \ 
- *                                  8(r)     													    8(r)   
+ *                                  \                                    \
+ *                                  8(r)     													    8(r)
  */
 void test_addRedBlackTree_add_8_to_tree_with_root_10_and_left_child_5_right_child_15(void)	{
 	setNode(&node5, NULL, NULL, 'r');
@@ -262,13 +262,13 @@ void test_addRedBlackTree_add_8_to_tree_with_root_10_and_left_child_5_right_chil
 	setNode(&node8, NULL, NULL, 'r');
 	setNode(&node10, &node5, &node15, 'b');
 	Node *root = &node10;
-	
+
 	printf("Start test_addRedBlackTree_add_8_to_tree_with_root_10_and_left_child_5_right_child_15\n");
 	addRedBlackTree(&root, &node8);
 	printf("-----------------------------");
-  
+
 	TEST_ASSERT_EQUAL_PTR(root, &node10);
-	
+
 	TEST_ASSERT_EQUAL_NODE(&node5, &node15, 'r', &node10);
 	TEST_ASSERT_EQUAL_NODE(NULL, &node8, 'b', &node5);
 	TEST_ASSERT_EQUAL_NODE(NULL, NULL, 'r', &node8);
